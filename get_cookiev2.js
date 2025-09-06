6ca96e6b197976c85954881225e14ee0568330df
patriksmith415 <118434911+patriksmith415@users.noreply.github.com>
2025-09-06 22:18:25 +0530

    Session Extraction

---
+++ get_cookiev2.js
@@ -0,0 +1,25 @@
+(function() {
+    const authtoken = localStorage.getItem("authtoken");
+    const userid = localStorage.getItem("userid");
+
+    
+    const cartString = localStorage.getItem("cart_info");
+    const regex = /"id":(\d+)/;
+    const match = regex.exec(cartString);
+    var cartId = ''
+    if (match && match.length > 1) {
+        cartId = match[1];
+        console.log(`Extracted Cart ID: ${cartId}`);
+    } else {
+        console.log("No cart ID found in the string.");
+    }
+
+    const result = {
+        'userid': userid,
+        'authtoken': authtoken,
+        'cart_id': cartId,
+    };
+
+    console.log(JSON.stringify(result, null, 2));
+
+})();
